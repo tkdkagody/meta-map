92d645b7c580bf68f4d545aedf6ac60774bf5081
tkdkagody <80307779+tkdkagody@users.noreply.github.com>
2023-05-08 18:27:40 +0900

    add: Add orbitController

---
+++ components/AnimatedBox.tsx
@@ -0,0 +1,19 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const AnimatedBox = () => {
+  const meshRef = useRef<THREE.Mesh>(null);
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+    }
+  });
+  return (
+    <mesh ref={meshRef} scale={[0.5, 0.5, 0.5]}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  );
+};
+
+export default AnimatedBox;
